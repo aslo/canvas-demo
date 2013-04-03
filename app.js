842cb689d54b1a8655150f3c98a1a2b1f47878a6
aslo <sloan623@gmail.com>
2013-04-03 19:14:50 -0400

    now looks for PORT env variable

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ var app = require('http').createServer(handler),
 var fileServer = new static.Server('./');
 	
 // This is the port for our web server.
-// you will need to go to http://localhost:8080 to see it
-app.listen(8080);
+var port = process.env.PORT || 8080;
+app.listen(port, function(){
+  console.log("Listening on port "+port);
+});
 
 // If the URL of the socket server is opened in a browser
 function handler (request, response) {
